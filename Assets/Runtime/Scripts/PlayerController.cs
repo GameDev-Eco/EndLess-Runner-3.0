7a81a7d611b8c7c8012a336c1577e9d6ae9e7e7e
GameDev-Eco <gamedev.eco@gmail.com>
2022-04-02 11:17:57 -0300

    vectors

---
+++ Assets/Runtime/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float horizontalSpeed = 0.03f;
+
+    private void Update() 
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * horizontalSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * horizontalSpeed;
+        }       
+        
+    }
+}
